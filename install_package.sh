71cdc1353553cee9cc334d8eadcc1c7d83623dad
Vu Le <v.vult5@vinid.net>
2020-03-21 19:23:04 +0700

    update install package

---
+++ install_package.sh
@@ -0,0 +1,11 @@
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get install -y build-essential \
+    cmake \
+    libboost-all-dev \
+    gcc \
+    libtinfo-dev \
+    zlib1g-dev \
+    clang-6.0 \
+    lldb-6.0 \
+    lld-6.0
